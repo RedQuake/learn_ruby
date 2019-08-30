d52b954ec15ffa7d1db14eb64cee31a8f3e6e667
Yan Chen <yanchen@Baby-Mac.local>
2019-08-30 14:30:49 +0100

    maximum a b find bigger number

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,14 @@
-def addfive(x,y)
-  return x * y - 7
-end
+# def addfive(x,y)
+#   return x * y - 7
+# end
+#
+# p addfive(6,55)
 
-p addfive(6,55)
+def maximum(a,b)
+  if (a > b)
+    return a
+  else
+    return b
+  end
+end
+p maximum(2,6) 
